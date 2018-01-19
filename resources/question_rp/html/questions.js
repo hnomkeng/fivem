159d182fc36eb9e20ab132ceadbd71e9e27ac234
Hnomkeng.Big <patcharapon.big@gmail.com>
2018-01-19 11:50:44 +0700

    [Added]question_rp
    https://github.com/freddy-nawfal/question_rp

---
+++ resources/question_rp/html/questions.js
@@ -0,0 +1,8 @@
+var tableauQuestion = [
+    {   question : "คุณกำลังเล่นโหมดอะไร ?",
+        propositionA : "Rond-point",
+        propositionB : "Role-play",
+        propositionC : "Relation publique",
+        propositionD : "Regle prioritaire",
+        reponse : "B"},
+]
